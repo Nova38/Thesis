87b5df31c4d2afc20897bd588527be99e3938834
Thomas <dev.csnova@gmail.com>
2023-10-27 05:17:05 -0500

    updated specimen contract

--- apps/chaincode/ccbio/v2/contract/specimen.go
+++ apps/chaincode/ccbio/v2/contract/specimen.go
@@ -1,22 +1,30 @@
 package contract
 
 import (
+	"github.com/bufbuild/protovalidate-go"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 	"github.com/hyperledger/fabric-contract-api-go/metadata"
 	"github.com/nova38/thesis/lib/go/fabric/rbac"
+	"github.com/nova38/thesis/lib/go/fabric/state"
 	pb "github.com/nova38/thesis/lib/go/gen/chaincode/ccbio/schema/v2"
+	rbac_pb "github.com/nova38/thesis/lib/go/gen/rbac"
+	"github.com/samber/oops"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
+
+var validator *protovalidate.Validator
 
 // SpecimenContract contract for handling BasicAssets
 type SpecimenContractImpl struct {
 	contractapi.Contract
+
 	pb.SpecimenServiceBase
 }
 
 // AuthServiceInterface
 
 var _ pb.SpecimenServiceInterface = (*SpecimenContractImpl)(nil)
+var _ contractapi.ContractInterface = (*SpecimenContractImpl)(nil)
 
 func BuildSpecimenContract() *SpecimenContractImpl {
 	return &SpecimenContractImpl{
@@ -43,6 +51,36 @@ func BuildSpecimenContract() *SpecimenContractImpl {
 
 // ────────────────────────────────────────────────────────────
 
+func (s *SpecimenContractImpl) GetBeforeTransaction() interface{} {
+	return s.BeforeTransaction
+}
+
+func (s *SpecimenContractImpl) BeforeTransaction(ctx rbac.AuthTxCtx) error {
+	// Check if the validate is initialized
+
+	if validator == nil {
+		v, err := protovalidate.New()
+		if err != nil {
+			panic(err)
+		}
+		validator = v
+	}
+
+	// Set the operations
+	ops, err := pb.SpecimenServiceGetTxOperation(ctx.GetFnName())
+	if err != nil {
+		return oops.
+			In("BeforeTransaction").
+			With("fn", ctx.GetFnName()).
+			Wrap(err)
+	}
+	if err := ctx.SetOperation(ops); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // ────────────────────────────────────────────────────────────
 // Query Functions
 // ────────────────────────────────────────────────────────────
@@ -51,8 +89,43 @@ func (s *SpecimenContractImpl) SpecimenGet(
 	ctx rbac.AuthTxCtx,
 	req *pb.SpecimenGetRequest,
 ) (res *pb.SpecimenGetResponse, err error) {
-	//TODO implement me
-	panic("implement me")
+	if err := validator.Validate(req); err != nil {
+		return nil, oops.
+			In(ctx.GetFnName()).
+			Code(rbac_pb.Error_ERROR_REQUEST_INVALID.String()).
+			Wrap(err)
+	}
+
+	if err := ctx.IsAuthorized(); err != nil {
+		return nil, oops.
+			In(ctx.GetFnName()).
+			Code(rbac_pb.Error_ERROR_USER_PERMISSION_DENIED.String()).
+			Wrap(err)
+	}
+
+	id := req.GetId()
+	if id == nil {
+		return nil, oops.
+			In(ctx.GetFnName()).
+			Code(rbac_pb.Error_ERROR_REQUEST_INVALID.String()).
+			Errorf("id is required")
+	}
+
+	specimen := &pb.Specimen{
+		Id: &pb.Specimen_Id{
+			CollectionId: id.CollectionId,
+			Id:           id.Id,
+		},
+	}
+	if err := state.GetState(&ctx, specimen); err != nil {
+		return nil, oops.
+			In(ctx.GetFnName()).
+			With("id", id).
+			Wrap(err)
+	}
+
+	return &pb.SpecimenGetResponse{Specimen: specimen}, nil
+
 }
 
 func (s *SpecimenContractImpl) SpecimenGetList(
@@ -187,5 +260,6 @@ func (s *SpecimenContractImpl) SuggestedUpdateReject(
 	req *pb.SuggestedUpdateRejectRequest,
 ) (res *pb.SuggestedUpdateRejectResponse, err error) {
 	//TODO implement me
+
 	panic("implement me")
 }
