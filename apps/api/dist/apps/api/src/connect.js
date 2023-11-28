9e43537fe2a0b8f164615ee021fbc78a394c77bc
Thomas N Atkins <dev.csnova@gmail.com>
2023-11-28 03:53:26 -0600

    remvoed dist

--- apps/api/dist/apps/api/src/connect.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var chaincodeConfig = {
-    peerEndpoint: 'peer0-org1.129.237.123.11.nip.io:443',
-    channel: 'demo',
-    contract: 'biochain',
-};
-var utf8Decoder = new TextDecoder();
-var client;
