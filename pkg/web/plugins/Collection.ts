f3978880036ab4f46efc780b07181446dd747979
Thomas N Atkins <dev.csnova@gmail.com>
2024-02-29 03:42:57 -0600

    feat: Added pugin for current collectionId

---
+++ pkg/web/plugins/Collection.ts
@@ -0,0 +1,28 @@
+export default defineNuxtPlugin({
+  name: 'Collection',
+  setup() {
+    const collectionId = useState('CurrentCollectionId', () => '')
+
+    addRouteMiddleware(
+      'collection',
+      (to, from) => {
+        const id = to.params?.collectionId
+
+        console.log('Collection middleware', to, from, id)
+
+        if (id && collectionId.value !== id) collectionId.value = id.toString()
+
+        console.log('Collection middleware', collectionId.value)
+      },
+      {
+        global: true,
+      },
+    )
+
+    return {
+      provide: {
+        collectionId,
+      },
+    }
+  },
+})
