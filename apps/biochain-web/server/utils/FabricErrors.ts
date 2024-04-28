6c2520cffae3ebb20e24d90d42be314e166cab65
Thomas Atkins <tom@ku.edu>
2024-04-27 22:09:43 -0500

    sync

--- apps/biochain-web/server/utils/FabricErrors.ts
+++ apps/biochain-web/server/utils/FabricErrors.ts
@@ -1,6 +1,34 @@
-import { GatewayError } from '@hyperledger/fabric-gateway'
+import { CommitError, GatewayError } from '@hyperledger/fabric-gateway'
+
+import { fabricErrors as FE } from '@saacs/client'
+
+
+function isCommitError(error: Error): error is CommitError {
+  return error instanceof CommitError
+}
 
 export default function HandleFabricError(error: unknown) {
-  if (error instanceof GatewayError) {
+
+  switch (true) {
+    case :
+      console.error('CommitError:', error)
+
+      break
+    case FE.isGatewayError(error):
+      console.error('GatewayError:', error)
+      break
+    case FE.isEndorseError(error):
+      console.error('EndorseError:', error)
+      break
+    case FE.isSubmitError(error):
+      console.error('SubmitError:', error)
+      break
+    case FE.isCommitStatusError(error):
+      console.error('CommitStatusError:', error)
+      break
+
+    case error instanceof Error:
+      console.error('UnknownError:', error)
+      break
   }
 }
--- libs/client/src/index.ts
+++ libs/client/src/index.ts
@@ -1,1 +1,3 @@
 export * as saacs from './saacs'
+
+export * as fabricErrors from './fabric/error'
